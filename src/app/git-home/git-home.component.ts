ae79402b9efa365b0a9d33b96337394487bf8019
Saitejas1997 <saitejas@apiplatform.io>
2021-12-10 08:16:28 +0530

    Implemented repository list section

--- src/app/git-home/git-home.component.ts
+++ src/app/git-home/git-home.component.ts
@@ -1,41 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ServiceService} from "../service.service";
 
 @Component({
-  selector: 'app-git-home',
-  templateUrl: './git-home.component.html',
-  styleUrls: ['./git-home.component.scss']
+    selector: 'app-git-home',
+    templateUrl: './git-home.component.html',
+    styleUrls: ['./git-home.component.scss']
 })
 export class GitHomeComponent implements OnInit {
 
-  userName = 'johnpapa';
-  userData: any;
-  repositories: any;
+    userName = 'johnpapa';
+    userData: any;
+    repositories: any;
 
-  page = 1;
-  itemsPerPage = 10;
-  reposURL = '';
+    page = 1;
+    itemsPerPage = 10;
+    reposURL = '';
 
-  constructor(private service: ServiceService) { }
+    constructor(private service: ServiceService) {
+    }
 
-  ngOnInit() {
-    this.getUser(this.userName);
-  }
+    ngOnInit() {
+        this.getUser(this.userName);
+    }
 
-  getUser(userName) {
-    this.service.getGitHubUser(userName).subscribe(response => {
-      if (response) {
-        this.userData = response;
-        if (this.userData && this.userData.hasOwnProperty('repos_url')) {
-          this.reposURL = this.userData['repos_url'];
-          this.getRepositories(this.page, this.itemsPerPage);
-        }
-      }
-    })
-  }
+    getUser(userName) {
+        this.service.getGitHubUser(userName).subscribe(response => {
+            if (response) {
+                this.userData = response;
+                if (this.userData && this.userData.hasOwnProperty('repos_url')) {
+                    this.reposURL = this.userData['repos_url'];
+                    this.getRepositories(this.page, this.itemsPerPage);
+                }
+            }
+        })
+    }
 
-  getRepositories(page, itemsPerPage) {
-    
-  }
+    getRepositories(page, itemsPerPage) {
+        const params = 'page=' + page + '&per_page=' + itemsPerPage;
+        this.service.getGitHubRepo(this.reposURL, params).subscribe(response => {
+            if (response) {
+                this.repositories = response;
+            }
+        });
+    }
 
 }
--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-repository',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repository.component.scss']
 })
 export class RepositoryComponent implements OnInit {
+
+  @Input() repository: any;
 
   constructor() { }
 
